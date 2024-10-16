cc549214dd935674b0237728950b092ab1c98b58
TOMCHRISTER\tomch <tomsch01266@stud.noroff.no>
2024-10-16 20:54:41 +0200

    fix: have put all test in single file

--- cypress/e2e/e2e-login-logout.cy.js
+++ cypress/e2e/e2e-login-logout.cy.js
@@ -1,21 +1,28 @@
 describe('Go to login page', () => {
-  before(() => {
-    cy.visit('http://127.0.0.1:57108');
+  beforeEach(() => {
+    // Visits this page before every test
+    cy.visit('http://127.0.0.1:8080');
   });
 
   it('Should log in with valid credentials', () => {
     cy.get('.btn-close').eq(1).click();
-    cy.wait(500);
-
-    cy.get('#loginEmail').type('workflowtest2@stud.noroff.no', { delay: 100 });
-    cy.wait(500);
-
-    cy.get('#loginPassword').type('Workflowtest1', { delay: 100 });
-    cy.wait(1000);
-
+    cy.get('#loginEmail').type('workflowtest2@stud.noroff.no');
+    cy.get('#loginPassword').type('Workflowtest1');
     cy.get('.btn-success').eq(1).click();
-    cy.wait(2500);
+  });
 
+  it('Should not be able to login with invalid credentials', () => {
+    cy.get('.btn-close').eq(1).click();
+    cy.get('#loginEmail').type('invalidemail@domain.com');
+    cy.get('#loginPassword').type('InvalidPassword');
+    cy.get('.btn-success').eq(1).click();
+  });
+
+  it('Should log out after logging in', () => {
+    cy.get('.btn-close').eq(1).click();
+    cy.get('#loginEmail').type('workflowtest2@stud.noroff.no');
+    cy.get('#loginPassword').type('Workflowtest1');
+    cy.get('.btn-success').eq(1).click();
     cy.get('button[data-auth="logout"]').should('be.visible').click();
   });
 });
